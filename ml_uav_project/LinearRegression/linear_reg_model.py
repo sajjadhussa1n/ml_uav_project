a8b0779de3715c50562cf6ea649230ec71820de4
sajjadhussa1n <sajjad.hussain2@seecs.edu.pk>
2024-04-11 21:09:08 +0500

    testing directory ownership issue

--- ml_uav_project/LinearRegression/linear_reg_model.py
+++ ml_uav_project/LinearRegression/linear_reg_model.py
@@ -1,6 +1,6 @@
 from ml_uav_project.EDA.preprocess import plot_model_results, show_model_results
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, mean_absolute_error
+from sklearn.metrics import root_mean_squared_error, mean_absolute_percentage_error, mean_absolute_error
 
 
 def linear_regression(X_train, y_train, X_test, y_test):
@@ -11,7 +11,7 @@ def linear_regression(X_train, y_train, X_test, y_test):
     #predicted_train = lin_reg.predict(X_train_scaled)
     #train_error = mean_squared_error(y_train,predicted_train,squared = False)
     predicted_test = lin_reg.predict(X_test)
-    test_error = mean_squared_error(y_test, predicted_test, squared = False)
+    test_error = root_mean_squared_error(y_test, predicted_test)
     print("Test RMSE error is: ", test_error, " dB.")
     r_squared_error_test = lin_reg.score(X_test, y_test)
     print("R-Squared error on test dataset is:",r_squared_error_test)
